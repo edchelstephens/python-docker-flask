a4da287c7ba23c9e98ea1f378fbd37901639f80f
Edchel Stephen Nini <edchelstephens@gmail.com>
2022-06-18 08:01:38 +0800

    logging: console handler.

--- loggers/logger.py
+++ loggers/logger.py
@@ -1,10 +1,14 @@
 import logging
 
-logging.basicConfig(
-    level=logging.DEBUG, format="%(asctime)s %(levelname)-8s %(name)-10s: %(message)s"
-)
+consoleHandler = logging.StreamHandler()
+
+formatter = logging.Formatter("%(asctime)s %(levelname)-8s %(name)-10s: %(message)s")
+consoleHandler.setFormatter(formatter)
 
 log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+log.addHandler(consoleHandler)
+
 log.debug("DEBUG message")
 log.info("INFO message")
 log.warning("WARNING message")
